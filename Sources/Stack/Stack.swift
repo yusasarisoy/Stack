84e58fd791e8726b97ce8a7e2739b46aba374a90
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-06-18 22:42:11 +0300

    Add Stack implementation

--- Sources/Stack/Stack.swift
+++ Sources/Stack/Stack.swift
@@ -1,6 +1,56 @@
-public struct Stack {
-    public private(set) var text = "Hello, World!"
+//
+//  Stack.swift
+//  Stack
+//
+//  Created by Yuşa on 18.06.2022.
+//
 
-    public init() {
-    }
+struct Stack<Element: Equatable>: Equatable {
+  /// Holds the stack data.
+  private var storage: [Element] = []
+  
+  init() { }
+  
+  /// Custom initializer for the **Stack**.
+  /// - Parameter elements: **Stack** elements to be added.
+  init(_ elements: [Element]) {
+    storage = elements
+  }
+  
+  /// Shows the last element of the stack if it exists.
+  /// - Returns: The last element of the stack or **nil** if the stack is empty.
+  func peek() -> Element? {
+    storage.last
+  }
+  
+  /// Checks whether the stack is empty.
+  /// - Returns: **true** or **false** based on the stack content.
+  func isEmpty() -> Bool {
+    peek() == nil // storage.isEmpty
+  }
+  
+  /// Provides to add a new element to the stack.
+  /// - Parameter element: The new element about to add.
+  mutating func push(_ element: Element) {
+    storage.append(element)
+  }
+  
+  /// Provides to remove the last element of the stack if it exists.
+  /// - Returns: The last element of the stack or **nil** if the stack is empty.
+  @discardableResult
+  mutating func pop() -> Element? {
+    storage.popLast()
+  }
+}
+
+extension Stack: CustomStringConvertible {
+  var description: String {
+    storage.map { "\($0)" }.joined(separator: " ")
+  }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+  init(arrayLiteral elements: Element...) {
+    storage = elements
+  }
 }
--- Tests/StackTests/StackTests.swift
+++ Tests/StackTests/StackTests.swift
@@ -1,11 +1,52 @@
+//
+//  StackTests.swift
+//  StackTests
+//
+//  Created by Yuşa on 18.06.2022.
+//
+
 import XCTest
 @testable import Stack
 
-final class StackTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Stack().text, "Hello, World!")
-    }
+final class StackTestCase: XCTestCase {
+  var stack = Stack<Int>()
+  
+  override func setUp() {
+    super.setUp()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+  }
+  
+  override func tearDown() {
+    stack = []
+    super.tearDown()
+  }
+  
+  func test_push() {
+    XCTAssertEqual(stack.description, "1 2 3 4")
+  }
+  
+  func test_pop() {
+    XCTAssertEqual(stack.pop(), 4)
+  }
+  
+  func test_peek() {
+    XCTAssertEqual(stack.peek(), 4)
+  }
+  
+  func test_isEmpty() {
+    XCTAssertFalse(stack.isEmpty())
+  }
+  
+  func test_initWithArray() {
+    let array = [1, 2, 3, 4]
+    XCTAssertEqual(stack, Stack(array))
+  }
+  
+  func test_arrayLiteral() {
+    let stack: Stack = ["bluberry", "plain", "potato"]
+    XCTAssertEqual(stack, ["bluberry", "plain", "potato"])
+  }
 }
